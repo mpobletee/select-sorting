e681f061a7dcb38a63ae4885d4845da2ec14fb31
mpobletee <mpoblete95@gmail.com>
2022-12-21 07:14:07 +0000

    sorting funcional en proceso de mejora

---
+++ instruction.md
@@ -0,0 +1,25 @@
+## 📝 Instructions  
+
+1.- Create a function that generates a list of random cards with suits.
+2.- Let the user specify how much random cards the website should generate using a text input.
+3.- Add a "draw" button that when clicked it renders those cards on the website in a beautiful way.
+4.- Add one "sort" button that sorts the cards using the bubble sorting algorithm.
+5.- Save all the changes that you hard to do while sorting the list of cards in a new array.
+6.- Display the entire log of changes one on top of each other.
+
+
+#
+```
+💡 Hint: 
+
+1.- Strategize first, no one starts coding the solution before having a clear strategy.
+
+2.- Stick to your strategy, forget about Stack Overflow for strategy.
+
+3.- Divide and conquer, try separating the exercise in smaller exercises, for example:
+
+3.1.- Make the hardcoded CSS and HTML before trying to make it dynamic, that will give you a clear sense of what HTML code you need to build with your algorithm.
+
+3.2.- Generate an array of random cards first, make sure is properly being generated (using the console.log) before trying to render it into the website.
+
+3.3.- Make a function just for building the HTML of ONE card and then re-use it to render all.
--- src/app.js
+++ src/app.js
@@ -2,10 +2,160 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let btnDraw = document.querySelector("#draw");
+  let btnSort = document.querySelector("#sort");
+  let amount = document.querySelector("#amount");
+  let contenedor = document.querySelector(".contenedor-cartas");
+  let bubbleDiv = document.querySelector(".bubbleSort");
+  let deck = [];
+
+  const crearDeck = () => {
+    let valores = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let tipos = ["Hearts", "Diamonds", "Spades", "Clubs"];
+
+    for (let i = 0; i < tipos.length; i++) {
+      for (let j = 0; j < valores.length; j++) {
+        const valor = valores[j];
+        const tipo = tipos[i];
+
+        deck.push({ valor, tipo });
+      }
+    }
+    return deck;
+  };
+
+  let randomCard = elem => {
+    const random = Math.floor(Math.random() * 51);
+    const valorCarta = deck[random].valor;
+    const tipoCarta = deck[random].tipo;
+
+    let icono = "";
+    if (tipoCarta === "Hearts") icono = "&hearts;";
+    if (tipoCarta === "Diamonds") icono = "&diams;";
+    if (tipoCarta === "Spades") icono = "&spades;";
+    if (tipoCarta === "Clubs") icono = "&clubs;";
+
+    const divCarta = document.createElement("div");
+    divCarta.classList.add("carta");
+    divCarta.innerHTML = `
+  <div class="header"> ${icono} </div> 
+  <div class="valor"> ${valorCarta} </div>
+  <div class="footer"> ${icono} </div>
+  `;
+    contenedor.appendChild(divCarta);
+    if (icono == "&hearts;" || icono == "&diams;") {
+      divCarta.classList.add("text-danger");
+    }
+  };
+
+  btnDraw.addEventListener("click", () => {
+    contenedor.innerHTML = "";
+    bubbleDiv.innerHTML = "";
+    let cantidad = amount.value;
+
+    if (cantidad > 0) {
+      for (let i = cantidad; i > 0; i--) {
+        randomCard(crearDeck());
+      }
+    }
+  });
+
+  btnSort.addEventListener("click", () => {
+    // bubbleDiv.innerHTML = contenedor.innerHTML;
+    let contenedorCarta = document.querySelector(".contenedor-cartas");
+    let grupoDivsCartas = contenedorCarta.childNodes;
+
+    let arrValor = [];
+    for (let i = 0; i < grupoDivsCartas.length; i++) {
+      let cuerpoDivCarta = grupoDivsCartas[i].textContent;
+      let expresionRegular = /\s*/;
+      let valorNumericoCarta = cuerpoDivCarta.trim();
+      const valorFinal = valorNumericoCarta.split(expresionRegular)[1];
+      if (valorFinal === "1") {
+        valorFinal = 10;
+      } else if (valorFinal === "A") {
+        valorFinal = 1;
+      } else if (valorFinal === "J") {
+        valorFinal = 11;
+      } else if (valorFinal === "Q") {
+        valorFinal = 12;
+      } else if (valorFinal === "K") {
+        valorFinal = 13;
+      } else if (valorFinal !== Number) {
+        valorFinal = valorFinal * 1;
+      }
+      arrValor.push(valorFinal);
+    }
+    // console.log(arrValor);
+
+    let bubbleSort = arr => {
+      let wall = arr.length - 1; //we start the wall at the end of the array
+      while (wall > 0) {
+        let index = 0;
+        while (index < wall) {
+          //compare the adjacent positions, if the right one is bigger, we have to swap
+          if (arr[index] > arr[index + 1]) {
+            let aux = arr[index];
+            arr[index] = arr[index + 1];
+            arr[index + 1] = aux;
+          }
+          index++;
+        }
+        wall--; //decrease the wall for optimization
+      }
+      return arr;
+    };
+
+    /* let sortListo = bubbleSort(arrValor);
+    let cc = document.querySelector(".contenedor-cartas");
+    let ccChildren = cc.childNodes;
+    for (let i = 0; i < ccChildren.length; i++) {
+      let dCartas = ccChildren[i];
+      console.log(sortListo);
+      console.log(dCartas);
+    } */
+
+    // let sortedFinal = randomCard(arrValor);
+    bubbleSort(arrValor);
+    arrValor.forEach(element => {
+      const divCartaSorted = document.createElement("div");
+      divCartaSorted.classList.add("carta");
+      // let newIcono = contenedor.childNodes[2];
+      // console.log(contenedor.childNodes);
+      if (element === 1) {
+        element = "A";
+      }
+      if (element === 11) {
+        element = "J";
+      }
+      if (element === 12) {
+        element = "Q";
+      }
+      if (element === 13) {
+        element = "K";
+      }
+      divCartaSorted.innerHTML = `
+      
+      <div class="valor"> ${element} </div>
+      `;
+
+      bubbleDiv.appendChild(divCartaSorted);
+    });
+  });
 };
